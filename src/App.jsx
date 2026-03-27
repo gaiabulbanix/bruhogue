963444d20de105d6d470897cf64c66c98f37f541
gaiabulbanix <frpcastillo@gmail.com>
2026-03-27 22:52:25 +0800

    feat: created top-level component Game.jsx and wired it to root component App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,22 @@
-import { useState } from 'react'
+// import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+// export default function App() {
+//   const [player, setPlayer] = useState({ x: 5, y: 5 });
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+//   return (
+//     <div>
+//       <h1>bruhogue</h1>
+//       <p>
+//         Player Position: ({player.x}, {player.y})
+//       </p>
+//     </div>
+//   );
+// }
 
-export default App
-
-import { useState } from 'react';
+import Game from "./components/Game";
 
 export default function App() {
-  const [player, setPlayer] = useState({ x: 5, y: 5 });
-
   return (
-    <div>
-      <h1>bruhogue</h1>
-      <p>
-        Player Position: ({player.x}, {player.y})
-      </p>
-    </div>
+    <Game />
   );
-} import { useState } from 'react';
-
-export default function App() {
-  const [player, setPlayer] = useState({ x: 5, y: 5 });
-
-  return (
-    <div>
-      <h1>bruhogue</h1>
-      <p>
-        Player Position: ({player.x}, {player.y})
-      </p>
-    </div>
-  );
-}
+};
---
+++ src/components/Game.jsx
@@ -0,0 +1,7 @@
+export default function Game() {
+    return (
+        <div>
+            <h1>Bruhogue</h1>
+        </div>
+    );
+};
