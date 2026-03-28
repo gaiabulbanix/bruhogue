2a828e62f92d64c561a24be4e7994bcb42cc4171
gaiabulbanix <frpcastillo@gmail.com>
2026-03-28 23:14:58 +0800

    feat: added useEffect hook for player movement including the handler inside for WSAD

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,3 @@
-// import { useState } from 'react';
-
-// export default function App() {
-//   const [player, setPlayer] = useState({ x: 5, y: 5 });
-
-//   return (
-//     <div>
-//       <h1>bruhogue</h1>
-//       <p>
-//         Player Position: ({player.x}, {player.y})
-//       </p>
-//     </div>
-//   );
-// }
-
 import Game from "./components/Game";
 
 export default function App() {
